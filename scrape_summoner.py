3297ff18b5f23220ab053d3217035ccb2d7b3181
Cole <coler1999@hotmail.com>
2020-01-29 00:59:40 -0500

    worked out some of the logic issues and implemented headless mode, also started accounting for remakes

--- scrape_summoner.py
+++ scrape_summoner.py
@@ -22,38 +22,49 @@ def update_summoner(url: str)->None:
     """
     from selenium.webdriver import Chrome
     import time
+    # import options so we can make our webdriver headless
+    from selenium.webdriver.chrome.options import Options
 
+    # instantiate Options object
+    chrome_options = Options()
+    # add the argument headless to that object
+    chrome_options.add_argument("--headless")
     webdriver = "C:/Users/ColeR/Documents/chromedriver.exe"
-    driver = Chrome(webdriver)
+    # pass in the options objct we created and the path to our chromedriver
+    driver = Chrome(options=chrome_options, executable_path=webdriver)
     driver.get(url)
     button = driver.find_element_by_id('SummonerRefreshButton')
     button.click()
     # pause the program for 5 seconds to make sure the update goes through
-    time.sleep(5)
+    #time.sleep(10)
     driver.close()
 
 
-def get_cur_day(now_est:datetime)-> str:
+#now_est:datetime
+def get_game_day(game_time:datetime)-> str:
     """
+    #TODO: change this to get the day the game occurred adjusted with the 2am rule
     gets the current day at time of running this script,
     if time is less than 3am we count it as previous day checks if it is the
     first day of the month.
     returns a string containing the adjusted date in the format YYYY-MM-DD
     Examples:
-    >>> get_cur_day(datetime.strptime("2020-01-25 19:31:57", "%Y-%m-%d %H:%M:%S"))
+    >>> get_game_day(datetime.strptime("2020-01-25 19:31:57", "%Y-%m-%d %H:%M:%S"))
     '2020-01-25'
-    >>> get_cur_day(datetime.strptime("2020-01-25 01:31:57", "%Y-%m-%d %H:%M:%S"))
+    >>> get_game_day(datetime.strptime("2020-01-25 01:31:57", "%Y-%m-%d %H:%M:%S"))
     '2020-01-24'
-    >>> get_cur_day(datetime.strptime("2020-02-01 01:31:57", "%Y-%m-%d %H:%M:%S"))
+    >>> get_game_day(datetime.strptime("2020-02-01 01:31:57", "%Y-%m-%d %H:%M:%S"))
     '2020-01-31'
     """
     # this variable will hold the adjusted datetime.day value
     adj_cur_day = now_est
     cur_time = now_est.hour
+    #print("current time " + str(cur_time))
 
     # since we will count games played until 3am as the previous day
     if cur_time <= 2:
         adj_cur_day -= timedelta(days=1)
+    #print("current day " + str(adj_cur_day))
     return str(adj_cur_day.date())
 
 
@@ -61,8 +72,9 @@ def get_new_matches(url: str, last_check:str)-> int:
     """
     returns all the matches that have been played 30 minutes or less ago
     #TODO: when getting new matches also make a tally of the days w/l ratio
-    game_result = match.find('div', class_="GameResult")
-    print(game_result.text)
+
+    #TODO: check if the game result was a remake, if it was a remake it shouldnt
+    be added.
     """
     new_games = 0
     page = requests.get(url)
@@ -73,16 +85,23 @@ def get_new_matches(url: str, last_check:str)-> int:
 
     # GameItemWrap is the div that holds all the game info
     matches = results.find_all('div', "GameItemWrap")
-
+    print("last check was at: " + str(last_check))
     for match in matches:
         game_was_played = match.find("div", class_="TimeStamp")
         game_time = datetime.strptime(game_was_played.text, "%Y-%m-%d %H:%M:%S")
+
+        # this is to adjust for the timechange of the 14 hours korea is ahead
+        game_time_est = game_time - timedelta(hours=14)
         last_check_dt = datetime.strptime(last_check, "%Y-%m-%d %H:%M:%S")
+        game_result = match.find('div', class_="GameResult").text.strip()
 
-        if game_time < last_check_dt:
-            new_games += 1
-            print("new game found! ")
-
+        # if a game has occurred since the last check increment new_games
+        if game_time_est > last_check_dt:
+            if game_result != "Remake":
+                new_games += 1
+                print("new game found! ")
+                print("game time: " + str(game_time_est))
+            # print(str(game_time.date()) + str(game_time.time()))
     return new_games
 
 
@@ -114,7 +133,11 @@ class Sheet:
 
         cur_cell = self.sheet.find(cur_day)
         todays_games = self.sheet.cell(cur_cell.row, cur_cell.col + 1).value
-        self.sheet.update_cell(cur_cell.row, cur_cell.col + 1, str(int(todays_games) + new_games))
+        if todays_games:
+            self.sheet.update_cell(cur_cell.row, cur_cell.col + 1, str(int(todays_games) + new_games))
+        else:
+            self.sheet.update_cell(cur_cell.row, cur_cell.col + 1, str(new_games))
+
 
     def set_last_check(self, day, time):
         """
@@ -132,8 +155,8 @@ class Sheet:
 
 if __name__ == "__main__":
     sheet = Sheet()
-    summoner_names = ["noskillzallluck"]
-    # curr not in use by me = "hydrosq", "lonely+fayze", "skillzy", "spearagirl","noskillzallluck"
+    summoner_names = ["noskillzallluck", "hydrosq", "skillzy", "spearagirl"]
+    # curr not in use by me ="lonely+fayze"
 
     now_utc = datetime.now(timezone("UTC"))
     now_est = now_utc.astimezone(timezone("America/Toronto"))
@@ -142,10 +165,10 @@ if __name__ == "__main__":
     for summoner in summoner_names:
         url = "https://na.op.gg/summoner/userName=" + summoner
         update_summoner(url)
-        cur_day = get_cur_day(now_est)
+        cur_day = get_game_day(now_est)
         new_matches = get_new_matches(url, sheet.get_last_check())
 
         if new_matches:
             sheet.update_gsheet(cur_day, new_matches)
 
-        sheet.set_last_check(str(now_est.date()), str(now_est.time()))
+    sheet.set_last_check(str(now_est.date()), str(now_est.time()))
